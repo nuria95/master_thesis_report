9e4464df4b1eb2b3589798893fbdcc1ea318a87f
nuriaarmengol <nuria_a_u@msn.com>
2020-05-03 17:17:31 +0200

    Adding all my files

---
+++ images/quantile_loss_plot.py
@@ -0,0 +1,32 @@
+import matplotlib
+import numpy as np
+from matplotlib import pyplot as plt
+import cycler
+
+# n = 3
+# color = plt.cm.viridis(np.linspace(0, 1, n))
+# matplotlib.rcParams['axes.prop_cycle'] = cycler.cycler('color', color)
+
+# plt.rcParams["axes.prop_cycle"] = plt.cycler("color", plt.cm.tab20c.colors)
+# plt.rcParams["axes.prop_cycle"] = plt.cycler("color","viridis")
+def quantile_plot(q):
+    error=np.arange(-1,1,0.01)
+    
+    quantile_loss = error*(q-(error<0))
+    quantile_loss2 = np.maximum(q * error, (q - 1) * error)
+    # print(quantile_loss)
+    plt.figure(1)
+    plt.plot(error,quantile_loss, label=f'q={q}')
+    plt.legend()
+
+
+
+if __name__ == "__main__":
+    for q in [0.25, 0.5, 0.75, 0.9]:
+        quantile_plot(q=q)
+    plt.title('Quantile loss')
+    plt.xlabel(r'$y_{true}-y_{predict}$')
+    plt.ylabel('Loss')
+    plt.xticks(np.arange(-1, 1.5, step=0.5))
+    plt.show()
+    
